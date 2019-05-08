48af97d475c90cdbeb555d6301dd11ea991c114b
Frankzhac <Frank.hemle@gmail.com>
2019-05-08 12:24:52 -0400

    Post.js refactored to reflect likes section added

--- instagram-app/src/components/CommentSection/CommentSection.js
+++ instagram-app/src/components/CommentSection/CommentSection.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './comment.css';
 
 
 const CommentSection = props => {
   return (
     <div className="comment-text">
-      <span className="comment">{props.comment.text}</span>{' '}
-      <span className="user">-{props.comment.username}</span>
+      <span className="user">{props.comment.username}</span>
+      <span className="comment"> {props.comment.text}</span>{' '}
     </div>
   );
 };
--- instagram-app/src/components/PostsContainer/Post.js
+++ instagram-app/src/components/PostsContainer/Post.js
@@ -3,25 +3,56 @@ import CommentContainer from '../CommentSection/CommentContainer';
 import PostHeader from './PostHeader';
 import './PostContainer.css';
 
+import Likes from './Likes';
+import PropTypes from 'prop-types';
 
 
-const Post = props => {
-  return (
-    <div className="P-border">
-      <PostHeader
-        username={props.post.username}
-        thumbnailUrl={props.post.thumbnailUrl}
-      />
-      <div className="P-img-wrapper">
-        <img
-          alt="post thumbnail"
-          className="P-img"
-          src={props.post.imageUrl}
+
+
+class Post extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      likes: props.post.likes
+    };
+  }
+  incrementLike = () => {
+    let likes = this.state.likes + 1;
+    this.setState({ likes });
+  };
+  render() {
+    return (
+      <div className="P-border">
+        <PostHeader
+          username={this.props.post.username}
+          thumbnailUrl={this.props.post.thumbnailUrl}
+        />
+        <div className="P-img-wrapper">
+          <img
+            alt="post thumbnail"
+            className="P-img"
+            src={this.props.post.imageUrl}
+          />
+        </div>
+        <Likes
+          incrementLike={this.incrementLike}
+          likes={this.state.likes}
+        />
+        <CommentContainer
+          postId={this.props.post.imageUrl}
+          comments={this.props.post.comments}
         />
       </div>
-      <CommentContainer comments={props.post.comments} />
-    </div>
-  );
+    );
+  }
+}
+
+Post.propTypes = {
+  post: PropTypes.shape({
+    username: PropTypes.string,
+    thumbnailUrl: PropTypes.string,
+    imageUrl: PropTypes.string
+  })
 };
 
 export default Post;
