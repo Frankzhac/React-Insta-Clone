38969242c8e4e2d5827f58a568b9b8de7bcb6fe2
Frankzhac <Frank.hemle@gmail.com>
2019-05-08 17:50:25 -0400

    PostPage

--- instagram-app/src/components/PostsContainer/PostPage.js
+++ instagram-app/src/components/PostsContainer/PostPage.js
@@ -1,1 +1,46 @@
+import React from 'react';
+import dummyData from './dummy-data';
+import PostContainer from './components/PostsContainer/PostContainer';
+import SearchBar from '.component/SearchBar';
 
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: [],
+      filteredPosts: []
+    };
+  }
+  componentDidMount() {
+    this.setState({ posts: dummyData });
+  }
+  searchPostsHandler = e => {
+    const posts = this.state.posts.filter(p => {
+      if (p.username.includes(e.target.value)) {
+        return p;
+      }
+    });
+    this.setState({ filteredPosts: posts });
+  };
+  render() {
+    return (
+      <div className="App">
+
+        <SearchBar
+          searchTerm={this.state.searchTerm}
+          searchPosts={this.searchPostsHandler}
+        />
+        <PostContainer
+          posts={
+            this.state.filteredPosts.length > 0
+              ? this.state.filteredPosts
+              : this.state.posts
+          }
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
