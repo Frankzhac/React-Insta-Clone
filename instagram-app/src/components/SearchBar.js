3918c7268e97bb64b34b64e2748494f91a4972dd
Frankzhac <Frank.hemle@gmail.com>
2019-05-08 09:45:11 -0400

    searchBar still not aligned correctly

--- instagram-app/src/components/SearchBar.js
+++ instagram-app/src/components/SearchBar.js
@@ -1,5 +1,7 @@
 import React from "react";
 import './SearchBar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 class SearchBar extends React.Component {
   render() {
@@ -7,10 +9,23 @@ class SearchBar extends React.Component {
       <nav className="Nav">
         <div className="Nav-menus">
           <div className="Nav-brand">
-            <a className="Nav-brand-logo" href="/">
-              Instagram
-            </a>
+            <a className="Nav-brand-logo" href="/"></a>
           </div>
+          <div>
+            <FontAwesomeIcon icon="search" />
+            <input type="text" placeholder="Search" />
+          </div>
+          <div className="social-container">
+            <div className="social">
+              <i className="fa fa-compass" />
+            </div>
+            <div className="social">
+              <i className="fa fa-heart" />
+            </div>
+            <div className="social">
+             <i className="fa fa-user-circle" />
+            </div>
+         </div>
         </div>
       </nav>
     );
